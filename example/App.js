5e9ec617f3c8ba27b6145464e76c0c10500eae2f
Raj <2112082@cognizant.com>
2023-10-06 00:12:35 -0700

    first push of code

---
+++ example/App.js
@@ -0,0 +1,16 @@
+const parent = React.createElement(
+  "div",
+   {id: "parent" },
+  React.createElement(
+    "div",
+    {id: "child"},
+    React.createElement(
+      "h1",{},"i am the h1 tag"
+      )
+  )
+);
+console.log(parent);
+
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
